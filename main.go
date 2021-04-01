448af2098efb37b2df67fdbd6a4301304b76bb67
lumi <leila.elkhalidy@gmail.com>
2021-04-01 00:59:32 -0600

    implement business logic

--- main.go
+++ main.go
@@ -5,20 +5,47 @@ import (
   "bufio"
   "os"
 	"github.com/luminaire-dev/velocity-limits/processor"
+  "log"
 )
 
 func main() {
+  // open input file
   file, err := os.Open("./input.txt")
     if err != nil {
         panic(err)
     }
     defer file.Close()
 
+  // create output file
+  dest, err := os.Create("./generated_output.txt")
+  	if err != nil {
+  		log.Fatal(err)
+  	}
+  	defer dest.Close()
+
+    // read from input file
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
-      res:= processor.Process(scanner.Bytes(), )
-      fmt.Println(res)
+      res:= processor.Process(scanner.Bytes())
+
+      // ignore duplicate IDs
+  		if res == nil {
+  			continue
+  		}
+
+      /// write to output file
+      _, err = fmt.Fprintln(dest, string(res))
+      if err != nil {
+        log.Fatal(err)
+      }
     }
-    //TODO: generate output file
-		fmt.Println("Done!")
+
+    // get file path and print location of output file
+    path, err := os.Getwd()
+    if err != nil {
+        log.Println(err)
+    }
+
+		fmt.Println("Done! 🎉")
+    fmt.Println("Output file has been generated at " + path + "/generated_output")
 }
--- processor/processor.go
+++ processor/processor.go
@@ -3,14 +3,18 @@ package processor
 import (
 	"encoding/json"
 	"strconv"
-	"fmt"
 	"time"
 )
 
 const (
-	dailyLimit   = 500000
-	weeklyLimit  = 2000000
+	dailyLimit   = 5000
+	weeklyLimit  = 20000
 	maxLoadCount = 3
+)
+
+var (
+	approvedLoads = make(map[string][]Load)
+	processedLoads = make(map[string]Response)
 )
 
 type Load struct {
@@ -20,8 +24,14 @@ type Load struct {
 	Time       time.Time `json:"time"`
 }
 
-// Approves or rejects the incoming load based on limits
-func Process(load []byte) bool {
+type Response struct {
+	ID         string `json:"id"`
+	CustomerID string `json:"customer_id"`
+	Accepted   bool   `json:"accepted"`
+}
+
+// approve or reject the incoming load based on limits validation
+func Process(load []byte)[]byte {
 
 	// parse the JSON-encoded data
 	var incomingLoad Load
@@ -29,17 +39,96 @@ func Process(load []byte) bool {
 	err != nil {
 		panic(err)
 	}
-	fmt.Println(incomingLoad)
 
-	// parse load amount to float so we can use it mathematically
-	loadAmount, parseErr := strconv.ParseFloat(incomingLoad.LoadAmount[1:], 64)
+	// parse load amount to float to use it mathematically
+	incomingLoadAmount, parseErr := strconv.ParseFloat(incomingLoad.LoadAmount[1:], 64)
 	if parseErr != nil {
 	   panic(parseErr)
 	 }
 
-	if(loadAmount > 5000){
-	  return false
+	// reject any incoming load amount greater than 5000, as it already fails the daily limit check
+	if(incomingLoadAmount > dailyLimit){
+		return GenerateResponse(incomingLoad, false)
 	}
 
-	return true
+	// ignore load with duplicate ID
+	if(processedLoads[incomingLoad.ID].CustomerID == incomingLoad.CustomerID) {
+		return nil
+	}
+
+	// iterate over processed load to get daily and weekly data per customer
+	var totalDailyAmount float64 = 0;
+	var totalWeeklyAmount float64 = 0;
+	var transactionCount = 0;
+	for _, processedLoad := range approvedLoads[incomingLoad.CustomerID] {
+
+		// parse processed load amount to float to use it mathematically
+		processedLoadAmount, parseErr := strconv.ParseFloat(processedLoad.LoadAmount[1:], 64)
+		if parseErr != nil {
+			 panic(parseErr)
+		 }
+
+		// add daily total and incrment cout for same day loads
+		if(DoesDayMatch(incomingLoad.Time, processedLoad.Time)){
+			 totalDailyAmount += processedLoadAmount
+			 transactionCount ++
+		}
+
+		// add weekly total for same week loads
+	  if(DoesWeekMatch(incomingLoad.Time, processedLoad.Time)) {
+			totalWeeklyAmount += processedLoadAmount
+	  }
+	}
+
+	// reject load if daily transaction count is exceeded
+	if(transactionCount == maxLoadCount){
+		return GenerateResponse(incomingLoad, false)
+	}
+
+	// reject load if daily trasaction limit is exceeded
+	if(totalDailyAmount + incomingLoadAmount > dailyLimit){
+		return GenerateResponse(incomingLoad, false)
+	}
+
+	// reject load if weekly trasaction limit is exceeded
+	if(totalWeeklyAmount + incomingLoadAmount > weeklyLimit){
+		return GenerateResponse(incomingLoad, false)
+	}
+
+	// store load into approved loads
+	approvedLoads[incomingLoad.CustomerID] = append(approvedLoads[incomingLoad.CustomerID], incomingLoad)
+
+	return GenerateResponse(incomingLoad, true)
+}
+
+// generate the json response that will be added to the output file
+func GenerateResponse(load Load, accepted bool) []byte {
+	r := Response{
+		ID:         load.ID,
+		CustomerID: load.CustomerID,
+		Accepted:   accepted,
+	}
+
+	// store load into processed Loads
+	processedLoads[load.ID] = r
+
+	res, err := json.Marshal(&r)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+// check if two date are on the same day
+func DoesDayMatch(date1, date2 time.Time) bool {
+    y1, m1, d1 := date1.Date()
+    y2, m2, d2 := date2.Date()
+    return y1 == y2 && m1 == m2 && d1 == d2
+}
+
+// check if two dates are in the same week
+func DoesWeekMatch(incomingLoadTime, processedLoadTime time.Time) bool {
+		year, week := incomingLoadTime.ISOWeek()
+		year2, week2 := processedLoadTime.ISOWeek()
+		return year == year2 && week == week2
 }
